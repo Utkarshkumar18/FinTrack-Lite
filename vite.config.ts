4d62a569a7486e7616aae2b5758c74eb3e512c9a
Utkarshkumar18 <140555214+Utkarshkumar18@users.noreply.github.com>
2026-04-27 11:14:30 +0530

    Add files via upload

---
+++ vite.config.ts
@@ -0,0 +1,21 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import path from "path";
+
+// https://vitejs.dev/config/
+export default defineConfig(({ mode }) => ({
+  server: {
+    // bind only to localhost so the dev server isn't reachable from the network
+    host: "localhost",
+    port: 8080,
+    hmr: {
+      overlay: false,
+    },
+  },
+  plugins: [react()].filter(Boolean),
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+}));
